3654f50e798c8747960e1e72c58c48a74988a291
mfigueras <mfigueras11@gmail.com>
2024-09-14 17:52:47 +0200

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: "https://mfiguera.github.io/",
-  base: "portfolio",
+  site: "https://mfiguera.github.io/portfolio",
+  base: ".",
 });
